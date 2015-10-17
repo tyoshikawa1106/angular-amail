1cd48fea961d8fc88b8fbdf2dac0a69246a318df
Taiki <tyoshikawa1106@gmail.com>
2015-10-18 01:58:05 +0900

    commit

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -1,36 +1,9 @@
-angular.module("Controllers", [])
+angular.module("Controllers", ["Services"])
 // ListController
-.controller("ListController", ["$scope", function($scope){
-  $scope.messages = getMessages();
+.controller("ListController", ["$scope", "MessageData", function($scope, MessageData){
+  $scope.messages = MessageData.all();
 }])
 // DetailController
-.controller("DetailController", ["$scope", "$routeParams", function($scope, $routeParams){
-  $scope.message = getMessages()[$routeParams.id];
+.controller("DetailController", ["$scope", "MessageData", "$routeParams", function($scope, MessageData, $routeParams){
+  $scope.message = MessageData.all()[$routeParams.id];
 }]);
-
-function getMessages() {
-  var messages = [{
-    id: 0,
-    sender: 'jean@somecompany.com', subject: 'Hi there, old friend',
-    date: 'Dec 7, 2013 12:32:00', recipients: ['greg@somecompany.com'],
-    message: 'Hey, we should get together for lunch sometime and catch up.'
-      +'There are many things we should collaborate on this year.'
-  },
-  {
-    id: 1,
-    sender: 'maria@somecompany.com',
-    subject: 'Where did you leave my laptop?',
-    date: 'Dec 7, 2013 8:15:12', recipients: ['greg@somecompany.com'],
-    message: 'I thought you were going to put it in my desk drawer.'
-      +'But it does not seem to be there.'
-  },
-  {
-    id: 2,
-    sender: 'bill@somecompany.com', subject: 'Lost python',
-    date: 'Dec 6, 2013 20:35:02', recipients: ['greg@somecompany.com'],
-    message: 'Nobody panic, but my pet python is missing from her cage.'
-      +'She doesn\'t move too fast, so just call me if you see her.'
-  }];
-
-  return messages;
-}
